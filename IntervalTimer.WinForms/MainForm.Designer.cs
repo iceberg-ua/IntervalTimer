2675e7e642fa6f5af995e85db97be3ee7ceb121e
iceberg <wladimir.melnik@gmail.com>
2024-06-02 17:37:26 +0200

    improve winforms application design

--- IntervalTimer.WinForms/MainForm.Designer.cs
+++ IntervalTimer.WinForms/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace IntervalTimer.WinForms;
 
-partial class Form1
+partial class MainForm
 {
     /// <summary>
     ///  Required designer variable.
@@ -41,70 +41,87 @@ partial class Form1
         // _activeTimerLabel
         // 
         _activeTimerLabel.AutoSize = true;
-        _activeTimerLabel.Location = new Point(187, 83);
+        _activeTimerLabel.Font = new Font("Trebuchet MS", 72F, FontStyle.Bold, GraphicsUnit.Point, 204);
+        _activeTimerLabel.ForeColor = Color.IndianRed;
+        _activeTimerLabel.Location = new Point(12, 9);
         _activeTimerLabel.Name = "_activeTimerLabel";
-        _activeTimerLabel.Size = new Size(34, 15);
+        _activeTimerLabel.Size = new Size(309, 119);
         _activeTimerLabel.TabIndex = 0;
         _activeTimerLabel.Text = "00:00";
         // 
         // _breakTimerLabel
         // 
         _breakTimerLabel.AutoSize = true;
-        _breakTimerLabel.Location = new Point(264, 83);
+        _breakTimerLabel.Font = new Font("Trebuchet MS", 72F, FontStyle.Bold, GraphicsUnit.Point, 204);
+        _breakTimerLabel.ForeColor = Color.DarkSeaGreen;
+        _breakTimerLabel.Location = new Point(327, 9);
         _breakTimerLabel.Name = "_breakTimerLabel";
-        _breakTimerLabel.Size = new Size(34, 15);
+        _breakTimerLabel.Size = new Size(309, 119);
         _breakTimerLabel.TabIndex = 1;
         _breakTimerLabel.Text = "00:00";
         // 
         // _startButton
         // 
-        _startButton.Location = new Point(165, 110);
+        _startButton.BackColor = Color.ForestGreen;
+        _startButton.FlatAppearance.BorderSize = 0;
+        _startButton.FlatStyle = FlatStyle.Flat;
+        _startButton.Font = new Font("Trebuchet MS", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+        _startButton.ForeColor = SystemColors.ButtonHighlight;
+        _startButton.Location = new Point(161, 165);
         _startButton.Name = "_startButton";
-        _startButton.Size = new Size(75, 23);
+        _startButton.Size = new Size(160, 60);
         _startButton.TabIndex = 2;
-        _startButton.Text = "start";
-        _startButton.UseVisualStyleBackColor = true;
+        _startButton.Text = "START";
+        _startButton.UseVisualStyleBackColor = false;
         _startButton.Click += OnStartButtonClick;
         // 
-        // _activeSpan
+        // _activeSpanCtrl
         // 
-        _activeSpanCtrl.Location = new Point(165, 57);
+        _activeSpanCtrl.Location = new Point(246, 131);
         _activeSpanCtrl.Maximum = new decimal(new int[] { 3600, 0, 0, 0 });
-        _activeSpanCtrl.Name = "_activeSpan";
+        _activeSpanCtrl.Name = "_activeSpanCtrl";
         _activeSpanCtrl.Size = new Size(75, 23);
         _activeSpanCtrl.TabIndex = 3;
         // 
-        // _breakSpan
+        // _breakSpanCtrl
         // 
-        _breakSpanCtrl.Location = new Point(246, 57);
+        _breakSpanCtrl.Location = new Point(327, 131);
         _breakSpanCtrl.Maximum = new decimal(new int[] { 3600, 0, 0, 0 });
-        _breakSpanCtrl.Name = "_breakSpan";
+        _breakSpanCtrl.Name = "_breakSpanCtrl";
         _breakSpanCtrl.Size = new Size(75, 23);
         _breakSpanCtrl.TabIndex = 4;
         // 
         // _stopButton
         // 
-        _stopButton.Location = new Point(246, 110);
+        _stopButton.BackColor = Color.FromArgb(192, 0, 0);
+        _stopButton.FlatAppearance.BorderSize = 0;
+        _stopButton.FlatStyle = FlatStyle.Flat;
+        _stopButton.Font = new Font("Trebuchet MS", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+        _stopButton.ForeColor = SystemColors.ButtonHighlight;
+        _stopButton.Location = new Point(327, 165);
         _stopButton.Name = "_stopButton";
-        _stopButton.Size = new Size(75, 23);
+        _stopButton.Size = new Size(160, 60);
         _stopButton.TabIndex = 5;
-        _stopButton.Text = "stop";
-        _stopButton.UseVisualStyleBackColor = true;
+        _stopButton.Text = "STOP";
+        _stopButton.UseVisualStyleBackColor = false;
         _stopButton.Click += OnStopButtonClick;
         // 
-        // Form1
+        // MainForm
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(800, 450);
+        BackColor = Color.DimGray;
+        ClientSize = new Size(644, 241);
         Controls.Add(_stopButton);
         Controls.Add(_breakSpanCtrl);
         Controls.Add(_activeSpanCtrl);
         Controls.Add(_startButton);
         Controls.Add(_breakTimerLabel);
         Controls.Add(_activeTimerLabel);
-        Name = "Form1";
-        Text = "Form1";
+        FormBorderStyle = FormBorderStyle.FixedToolWindow;
+        Name = "MainForm";
+        SizeGripStyle = SizeGripStyle.Show;
+        Text = "Interval timer";
         ((System.ComponentModel.ISupportInitialize)_activeSpanCtrl).EndInit();
         ((System.ComponentModel.ISupportInitialize)_breakSpanCtrl).EndInit();
         ResumeLayout(false);
--- IntervalTimer.WinForms/MainForm.cs
+++ IntervalTimer.WinForms/MainForm.cs
@@ -2,19 +2,19 @@ using Timer = System.Windows.Forms.Timer;
 
 namespace IntervalTimer.WinForms;
 
-public partial class Form1 : Form
+public partial class MainForm : Form
 {
-    Timer _mainTimer = new();
-
-    static double _intStep = 1000;
+    static readonly double _intStep = 1000;
+    readonly Timer _mainTimer = new();
 
     TimeSpan _activeTimeSpan;
     TimeSpan _breakTimeSpan;
     TimeSpan _timeSpan;
     TimeSpan _step = TimeSpan.FromMilliseconds(_intStep);
     bool _active = true;
+    int _counter = 0;
 
-    public Form1()
+    public MainForm()
     {
         InitializeComponent();
 
@@ -52,6 +52,7 @@ public partial class Form1 : Form
         _breakTimerLabel.Text = $"{_breakTimeSpan.Minutes,2:00}:{_breakTimeSpan.Seconds,2:00}";
 
         _timeSpan = _activeTimeSpan;
+        _counter = 0;
         _mainTimer.Start();
 
         _startButton.Enabled = false;
@@ -62,6 +63,9 @@ public partial class Form1 : Form
     private void OnStopButtonClick(object sender, EventArgs e)
     {
         _mainTimer.Stop();
+
+        _activeTimerLabel.Text = $"{_activeTimeSpan.Minutes,2:00}:{_activeTimeSpan.Seconds,2:00}";
+        _breakTimerLabel.Text = $"{_breakTimeSpan.Minutes,2:00}:{_breakTimeSpan.Seconds,2:00}";
 
         _startButton.Enabled = true;
         _activeSpanCtrl.Enabled = true;
